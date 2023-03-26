d6ee604a60064b9b189d8d6acab11c0b7404361b
Ririn Indah Cahyani <98936350+ririnindah@users.noreply.github.com>
2023-03-26 21:59:24 +0700

    Create IArea.java

---
+++ Praktikum 5/IArea.java
@@ -0,0 +1,13 @@
+/*
+ * File    : IArea.java 26/03/2023
+ * Penulis : Ririn Indah Cahyani - 24060121130069
+ * Lab     : PBO B1
+ * Deskripsi : interface yang mendefinisikan kontrak bahwa setiap kelas yang membuat implementasi
+ *             dari interface ini harus memiliki cara untuk menghitung luas
+ */
+
+package org;
+
+public interface IArea {
+    public double hitungLuas();
+}
