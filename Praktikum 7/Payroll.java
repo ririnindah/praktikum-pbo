225970a0f64a8d35fc95eb3dc765661be8fe14f2
Ririn Indah Cahyani <98936350+ririnindah@users.noreply.github.com>
2023-05-04 22:15:54 +0700

    Create Payroll.java

---
+++ Praktikum 7/Payroll.java
@@ -0,0 +1,14 @@
+/*
+ * File    : Payroll.java 04/05/2023
+ * Penulis : Ririn Indah Cahyani - 24060121130069
+ * Lab     : PBO B1
+ * Deskripsi : representasi dari objek Payroll yang berisi procedure cetakGaji
+ */
+
+package com;
+
+public class Payroll {
+    public void cetakGaji(Pegawai p) {
+        p.tampilData();
+    }
+}
